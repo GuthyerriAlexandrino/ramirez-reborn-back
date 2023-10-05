60e8b316d7b3164b0c8b6fa787c812876c40e584
odiunus <falcaofilho20@gmail.com>
2023-10-05 19:49:26 -0300

    refactor : comentarios nos metodos

--- app/services/user_service.rb
+++ app/services/user_service.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-# Service reponsible for creating user views
+# Service responsible for creating user views
 module UserService
-  # Permitted parameters for user creation/update.
+  # Returns all user data
   # @return [Hash<Symbol, Hash<Array>]
   def self.all_permited
     [:name, :email, :photographer, :password, :password_confirmation,
@@ -11,7 +11,7 @@ module UserService
   end
 
   # Fields to view when fetching a user.
-  # @return [Hash<Symbol, Hash<Array>]
+  # @return [Hash<Symbol, Hash<Symbol>]
   def self.search_view
     %i[name email profile_img specialization services_price city state views bio]
   end
