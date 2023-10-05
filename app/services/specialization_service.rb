5bacf10f67902bcebd2f3490b60810d3fca44fd5
marialicemacedo <marialicemacedo@alu.ufc.br>
2023-10-04 21:54:52 -0300

    Specialization service constructor

--- app/services/specialization_service.rb
+++ app/services/specialization_service.rb
@@ -9,4 +9,18 @@ class SpecializationService
     @instance ||= SpecializationService.new
   end
 
-  
+  private
+  # Constructor: initializes a new instance and builds specializations
+  def initialize
+    @specializations = build_specializations()
+  end
+
+  # Method to build the list of specializations
+  # @return [Hash]
+  def build_specializations()
+    sps = Set.new()
+    sp = Specialization.all()
+    sp.each { |s| sps << s[:name] }
+    sps
+  end
+end
