935e2bd657cd977ae30df580796c74a2552aef67
marialicemacedo <marialicemacedo@alu.ufc.br>
2023-10-05 12:26:46 -0300

    Specialization controller

--- app/controllers/specializations_controller.rb
+++ app/controllers/specializations_controller.rb
@@ -5,7 +5,6 @@ class SpecializationsController < ApplicationController
   before_action :set_specialization, only: %i[show update destroy]
 
   # GET /specializations
-  # @param specializations [String]
   def index
     @specializations = Specialization.all
     render json: @specializations
@@ -16,7 +15,39 @@ class SpecializationsController < ApplicationController
     render json: @specialization
   end
 
+  # POST /specializations
+  def create
+    @specialization = Specialization.new(specialization_params)
+
+    if @specialization.save
+      render json: @specialization, status: :created, location: @specialization
+    else
+      render json: @specialization.errors, status: :unprocessable_entity
     end
   end
 
+  # PATCH/PUT /specializations/1
+  def update
+    if @specialization.update(specialization_params)
+      render json: @specialization
+    else
+      render json: @specialization.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /specializations/1
+  def destroy
+    @specialization.destroy
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_specialization
+      @specialization = Specialization.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def specialization_params
+      params.require(:specialization).permit(:name, :string)
+    end
 end
