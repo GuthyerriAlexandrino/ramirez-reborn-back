984653add3454513c56d117298bb75bec5202f19
marialicemacedo <marialicemacedo@alu.ufc.br>
2023-10-06 16:36:37 -0300

    Fixes style

--- app/services/specialization_service.rb
+++ app/services/specialization_service.rb
@@ -1,25 +1,28 @@
+# frozen_string_literal: true
+
 # Singleton class
 class SpecializationService
   # Constant: List of specializations
   attr_reader :specializations
-  
+
   # Method to get the singleton instance of SpecializationService
   # @return [SpecializationService]
-  def SpecializationService.instance
+  def self.instance
     @instance ||= SpecializationService.new
   end
 
   private
+
   # Constructor: initializes a new instance and builds specializations
   def initialize
-    @specializations = build_specializations()
+    @specializations = build_specializations
   end
 
   # Method to build the list of specializations
   # @return [Hash]
-  def build_specializations()
-    sps = Set.new()
-    sp = Specialization.all()
+  def build_specializations
+    sps = Set.new
+    sp = Specialization.all
     sp.each { |s| sps << s[:name] }
     sps
   end
