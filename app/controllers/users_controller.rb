bb6fa67f93c13053085e62f2976c2d56a80f75b6
odiunus <falcaofilho20@gmail.com>
2023-10-06 21:27:41 -0300

    feat : controler user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,13 +3,16 @@
 # Class controller user
 class UsersController < ApplicationController
   ActionController::Parameters.action_on_unpermitted_parameters = :raise
-  before_action :authorize_request, except: [:create, :update, :show]
+  before_action :authorize_request, except: %i[create update show]
   before_action :set_user, only: :show
 
   # GET /users
   # Search all users by filter
   def index
-    return render json: { error: 'Page field must be integer' }, status: :bad_request unless FiltersService.check_pagination(params[:page])
+    unless FiltersService.check_pagination(params[:page])
+      return render json: { error: 'Page field must be integer' },
+                    status: :bad_request
+    end
 
     filters = FiltersService.matching_params(request.GET)
     location = FiltersService.location_params(request.GET[:location])
@@ -30,14 +33,12 @@ class UsersController < ApplicationController
     render json: user, status: :ok
   end
 
-
   # GET user/1/
   # Search photographers that the user is following
   def following
     photographer = get_photographer
 
     render json: photographer.following, status: :ok
-
   rescue Mongoid::Errors::InvalidFind
     render json: { error: 'Invalid photographer' }, status: :bad_request
   rescue Mongoid::Errors::MongoidError => e
@@ -62,5 +63,4 @@ class UsersController < ApplicationController
       render json: { error: user.errors }, status: :unprocessable_entity
     end
   end
-
-  end
+end
