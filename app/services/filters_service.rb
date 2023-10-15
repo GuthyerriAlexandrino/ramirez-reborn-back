f06520a9234526605e8e5962d33ff9aee9befb9b
ewaldojunior <ewaldoj18@gmail>
2023-10-15 16:24:13 -0300

    feat: Add documentation "check_param"

--- app/services/filters_service.rb
+++ app/services/filters_service.rb
@@ -2,6 +2,8 @@
 
 module FiltersService
 
+  # Class method that checks whether a key-value pair meets specific conditions
+  # @return [Boolean]
   def self.check_param(key, value)
     key = key.to_sym
     condition1 = %i[name specialization].include?(key)
@@ -10,6 +12,7 @@ module FiltersService
     condition1 && condition2
   end
 
+  # Defines the "check_param" method as a private class method
   private_class_method :check_param
 
 end
