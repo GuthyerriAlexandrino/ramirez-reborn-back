f766f178fc8a2992926dddfa312fc54098f8be6d
ewaldojunior <ewaldoj18@gmail>
2023-10-15 17:05:16 -0300

    feat: Adds more documentation

--- app/services/filters_service.rb
+++ app/services/filters_service.rb
@@ -1,14 +1,21 @@
 # frozen_string_literal: true
-
+# This module encapsulates filter-related functionality
 module FiltersService
 
   # Class method that checks whether a key-value pair meets specific conditions
   # @return [Boolean]
   def self.check_param(key, value)
+
+    #Constant Symbols: Defines a symbol with the key name to facilitate comparison
     key = key.to_sym
+
+    #Constant Symbols: Condition 1 - Checks if the key is included in the symbol array
     condition1 = %i[name specialization].include?(key)
+
+    #Constant String: Condition 2 - Checks if the value is not an empty string
     condition2 = value != "" && !value.nil?
 
+    #Class Method: Checks if both conditions are true
     condition1 && condition2
   end
 
