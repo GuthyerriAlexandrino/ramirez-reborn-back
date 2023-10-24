32e71a2ba467d51ccee8b24084054685f4f4c9af
marialicemacedo <marialicemacedo@alu.ufc.br>
2023-10-24 09:41:42 -0300

    connection

--- app/services/fire_storage_service.rb
+++ app/services/fire_storage_service.rb
@@ -6,3 +6,10 @@ class FireStorageService
   def FireStorageService.instance
     @instance ||= FireStorageService.new
   end
+
+  private
+  def initialize
+    @storage = Google:Cloud::Storage.new project_id: 'ramirez-2bb46', credentials: GOOGLE_APPLICATION_CREDENTIALS
+    @img_bucket = @storage.bucket('ramirez-2bb46.appspot.com)
+  end
+end
