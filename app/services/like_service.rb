960ec8bed2b921823411bdf84d18c31c4a87b656
ewaldojunior <ewaldoj18@gmail>
2023-11-07 15:58:59 -0300

    doc: Add documentation get_like

--- app/services/like_service.rb
+++ app/services/like_service.rb
@@ -2,6 +2,10 @@
 
 module LikeService
 
+  # Method responsible for obtaining the like given by a user in a comment or post
+  # @return [Object]
+  # @param user_id [Integer]
+  # @param likeable [Object]
   def self.get_like(user_id, likeable)
     return likeable.likes&.find(user_id) if likeable.instance_of?(Post)
 
