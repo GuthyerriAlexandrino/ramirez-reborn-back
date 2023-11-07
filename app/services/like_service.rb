b2c358f6ce59767915b7c457564dc95744ce7b23
ewaldojunior <ewaldoj18@gmail>
2023-11-07 16:10:04 -0300

    feat: Add method like

--- app/services/like_service.rb
+++ app/services/like_service.rb
@@ -11,4 +11,15 @@ module LikeService
 
     likeable.likes&.find(user_id) if likeable.instance_of?(Comment)
   end
+
+  def self.like(user_id, likeable)
+    like = get_like(user_id, likeable)
+    if like.nil?
+      likeable.likes.create!(user_id: user.id)
+      return { json: 'Object created', status: :ok }
+    end
+    like.destroy
+    { json: 'Object destroyed', status: :ok }
+  end
+
 end
