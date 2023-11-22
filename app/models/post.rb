52a87b475f38ed20387175436ef31a04b1d279e9
Wesley L. Pinheiro <wesleylpinheiro@gmail.com>
2023-11-22 16:56:26 -0300

    rubocop corrections

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,15 +2,15 @@
 
 # A Post object who represents the possible posts in the database
 class Post
-    include Mongoid::Document
-    include Mongoid::Timestamps
-    field :title, type: String
-    field :image, type: String
-    field :price, type: Float
-    embedded_in :user
-    embeds_many :likes
-    has_many :comments
-  
-    validates_length_of :title, minimum: 1, maximum: 160
-    validates_numericality_of :price, greater_than: 0, allow_nil: true
-  end
+  include Mongoid::Document
+  include Mongoid::Timestamps
+  field :title, type: String
+  field :image, type: String
+  field :price, type: Float
+  embedded_in :user
+  embeds_many :likes
+  has_many :comments
+
+  validates :title, length: { minimum: 1, maximum: 160 }
+  validates :price, numericality: { greater_than: 0, allow_nil: true }
+end
