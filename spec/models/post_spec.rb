d342aa699e17168313f0d95c1e3762f8806320de
Wesley L. Pinheiro <wesleylpinheiro@gmail.com>
2023-11-22 16:24:07 -0300

    adds post_spec fields tests

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,4 +1,8 @@
 RSpec.describe Post do
     it { is_expected.to be_mongoid_document }
+  
+    it { is_expected.to have_field(:title).of_type(String) }
+    it { is_expected.to have_field(:image).of_type(String) }
+    it { is_expected.to have_field(:price).of_type(Float) }
   end
   
