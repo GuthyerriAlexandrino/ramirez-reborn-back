0dadb99eff10c747004b4dd32d6feee018137b9e
Wesley L. Pinheiro <wesleylpinheiro@gmail.com>
2023-11-22 16:25:27 -0300

    adds post_spec validationn tests

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -4,5 +4,8 @@ RSpec.describe Post do
     it { is_expected.to have_field(:title).of_type(String) }
     it { is_expected.to have_field(:image).of_type(String) }
     it { is_expected.to have_field(:price).of_type(Float) }
+  
+    it { is_expected.to validate_length_of(:title).within(1..160) }
+    it { is_expected.to validate_numericality_of(:price).allow_nil.is_greater_than(0) }
   end
   
