e604540c887bc1e708501fd55bbaa413079f6e8d
Wesley L. Pinheiro <wesleylpinheiro@gmail.com>
2023-11-22 16:33:41 -0300

    adds returns an empty array if user has no posts test

--- spec/services/post_service_spec.rb
+++ spec/services/post_service_spec.rb
@@ -9,6 +9,15 @@ RSpec.describe PostService do
         expect(posts).to eq(user.posts)
       end
   
-      
+      it 'returns an empty array if user has no posts' do
+        user = create(:user)
+  
+        posts = PostService.user_posts(user.id.to_s)
+  
+        expect(posts).to be_empty
+      end
+    end
+  
+   
   end
   
