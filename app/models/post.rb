b42d018dc1b96a6f9291700b1d1f895f922d3626
Wesley L. Pinheiro <wesleylpinheiro@gmail.com>
2023-11-22 16:58:51 -0300

    fixes has_many in Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,7 +9,7 @@ class Post
   field :price, type: Float
   embedded_in :user
   embeds_many :likes
-  has_many :comments
+  has_many :comments, dependent: :destroy
 
   validates :title, length: { minimum: 1, maximum: 160 }
   validates :price, numericality: { greater_than: 0, allow_nil: true }
