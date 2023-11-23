6c5cacc31d26714bc4cf0faca77501c14447a0ab
GuthyerriAlexandrino <galexandrino4@gmail.com>
2023-11-23 13:27:38 -0300

    Fixes like service error

--- app/services/like_service.rb
+++ app/services/like_service.rb
@@ -3,21 +3,21 @@
 # This module encapsulates like-related functionalities
 module LikeService
   # Method responsible for obtaining the like given by a user in a comment or post
-  # @return [Object]
+  # @return [Like]
   # @param user_id [Integer]
-  # @param likeable [Object]
+  # @param likeable [Post,Comment]
   def self.get_like(user_id, likeable)
     likeable.likes&.find(user_id)
   end
 
   # Method responsible for allowing a user to add or remove a like
-  # @return [Hash]
+  # @return [Json]
   # @param user_id [Integer]
-  # @param likeable [Object]
+  # @param likeable [Post,Comment]
   def self.like(user_id, likeable)
     like = get_like(user_id, likeable)
     if like.nil?
-      likeable.likes.create!(user_id: user.id)
+      likeable.likes.create!(user_id:)
       return { json: 'Object created', status: :ok }
     end
     like.destroy
